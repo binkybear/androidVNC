d5f58a4afe9634ed8bc65a64e6c011859edb9dbe
Vladimir Panteleev <git@thecybershadow.net>
2016-07-20 16:30:41 +0300

    BCStorageContext8: Add @TargetApi annotation
    Fixes linter error

--- app/src/main/java/com/antlersoft/android/bc/BCStorageContext8.java
+++ app/src/main/java/com/antlersoft/android/bc/BCStorageContext8.java
@@ -5,7 +5,9 @@ package com.antlersoft.android.bc;
 
 import java.io.File;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 
 /**
  * @author Michael A. MacDonald
@@ -16,6 +18,7 @@ class BCStorageContext8 implements IBCStorageContext {
 	/* (non-Javadoc)
 	 * @see com.antlersoft.android.bc.IBCStorageContext#getExternalStorageDir(android.content.Context, java.lang.String)
 	 */
+	@TargetApi(Build.VERSION_CODES.FROYO)
 	@Override
 	public File getExternalStorageDir(Context context, String type) {
 		return context.getExternalFilesDir(type);
